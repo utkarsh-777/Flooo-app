2d8d25681e0397b3983d68a69d82a445ba60fedf
utkarsh-777 <kumarutkarsh305@gmail.com>
2020-09-02 03:53:18 +0530

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-const cors = require('cors')
+//const cors = require('cors')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 
@@ -10,7 +10,7 @@ const authRoutes = require("./routes/auth")
 const postRoutes = require('./routes/post')
 const {MONGOURI} = require('./config/keys')
 
-app.use(cors())
+//app.use(cors())
 app.use(bodyParser.json())
 
 mongoose.connect(MONGOURI,{
@@ -25,7 +25,6 @@ app.use('/',authRoutes)
 app.use('/',postRoutes)
 
 if(process.env.NODE_ENV=='production'){
-    app.use(cors())
     app.use(express.static('postapp/build'))
     const path = require('path')
     app.get("*",(req,res)=>{
